8ee57d044e9b7a7d1ee3d3aeaf0e8bfe100359a8
siddreddy07 <siddharthreddy627@gmail.com>
2025-07-14 20:49:11 +0530

    Added peerlist svg

--- project/src/pages/Homepage.jsx
+++ project/src/pages/Homepage.jsx
@@ -75,8 +75,11 @@ const Homepage = () => {
             </h1>
 
             <span className='md:hidden flex items-center gap-2 justify-center'>
-              <h1 className='text-lg bg-gradient-to-r hover:from-gray-400 hover:transition-all duration-500 animate-pulse ease-linear hover:to-white hover:cursor-default from-white to-gray-400 bg-clip-text text-transparent'>📢</h1>
-            <a href="https://www.producthunt.com/products/dbsmash?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-dbsmash" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=991628&theme=dark&t=1752420889217" alt="DbSmash - Schema&#0032;gen&#0032;&#0038;&#0032;init&#0032;via&#0032;smart&#0032;CLI&#0032;—&#0032;web&#0032;UI&#0032;if&#0032;you&#0039;re&#0032;lazy | Product Hunt" width="150" height="54" /></a>
+              <a href="https://peerlist.io/siddreddy/project/dbsmash">
+            <img src="../../public/peerlist.svg" width="120" height="54" alt="" />
+            </a>
+              <h1 className='text-md bg-gradient-to-r hover:from-gray-400 hover:transition-all duration-500 animate-pulse ease-linear hover:to-white hover:cursor-default from-white to-gray-400 bg-clip-text text-transparent'>📢</h1>
+            <a href="https://www.producthunt.com/products/dbsmash?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-dbsmash" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=991628&theme=dark&t=1752420889217" alt="DbSmash - Schema&#0032;gen&#0032;&#0038;&#0032;init&#0032;via&#0032;smart&#0032;CLI&#0032;—&#0032;web&#0032;UI&#0032;if&#0032;you&#0039;re&#0032;lazy | Product Hunt" width="120" height="54" /></a>
           </span>
           </motion.div>
 
